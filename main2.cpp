9fcf2b539f15cb96f3781f7a32c92805ef61fdb3
esther-mi <estheressien0805@gmail.com>
2026-09-22 16:03:15 +0100

    Add guessing game logic with attempt limits
    Implement a guessing game with limited attempts using while, do while and for loops.

--- main2.cpp
+++ main2.cpp
@@ -1,1 +1,28 @@
+#include iostream;
+using namespace std;
 
+int secret_num = 10;
+int guess;
+int guess_count = 0;
+int guess_limit = 3;
+int out_of_guesses = false;
+
+int main()
+{
+  do{
+    if (guess_count < guess_limit){
+      cout << "Enter guess" << endl;
+      cin >> guess;
+      guess_count++;
+    }else{
+      out_of_guesses = true;
+    }
+  } while(guess != secret_num && !out_of_guesses){
+    if(out_of_guesses){
+      cout << "You Loose";
+    }else{
+      cout << "You win";
+    }
+  }
+  return 0;
+}
